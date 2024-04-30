bc5f2a230b08923b93df149a93c5685bb92d4705
ivanlozaDEV <159062304+ivanlozaDEV@users.noreply.github.com>
2024-04-30 03:14:55 +0000

    added domain generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let extension = [".com", ".org", ".edu"];
+
+let generatedDomain = "";
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < extension.length; l++) {
+        generatedDomain = pronoun[i] + adj[j] + noun[k] + extension[l];
+        console.log(generatedDomain);
+      }
+    }
+  }
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const domain = document.getElementById("domainTarget");
+  domain.innerHTML = generatedDomain;
 };
